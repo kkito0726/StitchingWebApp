a92b4080f11f89723d0271b4d0de776341bb16b6
kkito0726 <k.kito0726@gmail.com>
2022-08-14 22:02:51 +0900

    first commit

---
+++ Documents/React/filepath/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FileReader from './FileReader';
+
+function App() {
+  return (
+    <div className="App">
+      <FileReader />
+    </div>
+  );
+}
+
+export default App;
